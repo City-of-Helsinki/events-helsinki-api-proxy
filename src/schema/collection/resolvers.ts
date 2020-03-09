00533aa141f79283708eef8c9136dd2eda31294a
Jori Lindell <jori.lindell@gmail.com>
2020-03-09 13:11:38 +0200

    Remove un-used mock data

--- src/schema/collection/resolvers.ts
+++ src/schema/collection/resolvers.ts
@@ -1,63 +1,6 @@
 import normalizeKeys from "../../utils/normalizeKeys";
 
-const mockCollection = {
-  curatedEvents: [
-    "http://localhost:3000/fi/event/helsinki:afxh3naida?id=123",
-    "http://localhost:3000/fi/event/helsinki:afxrsql3xa",
-    "http://localhost:3000/fi/event/helsinki:afxh3namhe",
-    "http://localhost:3000/fi/event/helsinki:afxpj6bxbu",
-    "http://localhost:3000/fi/event/helsinki:afx5msunhu"
-  ],
-  curatedEventsTitle: {
-    en: "At least visit these",
-    fi: "Käy ainakin näissä",
-    sv: "Besök åtminstone dessa"
-  },
-  description: {
-    en:
-      // eslint-disable-next-line max-len
-      "Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus.",
-    fi:
-      // eslint-disable-next-line max-len
-      "Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus.",
-    sv:
-      // eslint-disable-next-line max-len
-      "Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus."
-  },
-  eventListQuery:
-    "http://localhost:3000/en/events?categories=music&districts=kaupunginosa%3Aetu-t%C3%B6%C3%B6l%C3%B6",
-  eventListTitle: {
-    en: "All the best events of the fall",
-    fi: "Kaikki syksyn parhaat tapahtumat",
-    sv: "Höstens bästa händelser"
-  },
-  linkText: {
-    en: "Read more on the project website",
-    fi: "Lue lisää hankkeen omilta sivuilta",
-    sv: "Läs mer på projektets webbplats"
-  },
-  linkUrl: {
-    en: "http://www.google.com",
-    fi: "http://www.google.com",
-    sv: "http://www.google.com"
-  },
-  shortDescription: {
-    en: "We put together the best foliage for the fall",
-    fi: "Kokosimme parhaat tärpit syksylle",
-    sv: "Vi sätter ihop det bästa bladverket för hösten"
-  },
-  similarCollectionsTitle: {
-    en: "Check also these",
-    fi: "Katso myös nämä",
-    sv: "Se dessa också"
-  },
-  title: {
-    en: "The most bustling tricks of the fall",
-    fi: "Syksyn riehakkaimmat riennot",
-    sv: "Höstens mest livliga knep"
-  }
-};
-
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const normalizeCollection = (collection: any) => {
   const normalizedCollection = normalizeKeys(collection);
 
