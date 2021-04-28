d2c8d5e4aedbca1be390526eb98c3e0027c5e313
Santeri Rusila <santtu_1993@hotmail.com>
2021-04-28 08:22:35 +0300

    disable pretty print

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -5,7 +5,7 @@ const logger = winston.createLogger({
   silent: process.env.GRAPHQL_PROXY_DISABLE_WINSTON_LOGGING === 'true',
   transports: [
     new winston.transports.Console({
-      format: winston.format.prettyPrint(),
+      // format: winston.format.prettyPrint(),
     }),
     // Uncomment if you want to log to files, see: https://github.com/winstonjs/winston#usage
     // new winston.transports.File({
